af8788a7727b5f1b50d70704966fe58a2fde5e82
benchstyle1 <47250962+canijke@users.noreply.github.com>
2020-07-20 10:25:25 +0300

    deploy

---
+++ precache-manifest.f919941cb4555fe8cdbb7bb0e0ec0236.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc24e53ce9e31fa3c90",
+    "url": "/todoList/css/app.f49cccca.css"
+  },
+  {
+    "revision": "ba5c3e3d04ce2b2e6fef",
+    "url": "/todoList/css/chunk-1726e87e.666f8541.css"
+  },
+  {
+    "revision": "3f2a0b401dc355f4cb5d",
+    "url": "/todoList/css/chunk-411504aa.c4cd783e.css"
+  },
+  {
+    "revision": "99501502e38cc0ea3f6172b0011aa025",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "0dc24e53ce9e31fa3c90",
+    "url": "/todoList/js/app.c7c4ddca.js"
+  },
+  {
+    "revision": "ba5c3e3d04ce2b2e6fef",
+    "url": "/todoList/js/chunk-1726e87e.e14fab17.js"
+  },
+  {
+    "revision": "3f2a0b401dc355f4cb5d",
+    "url": "/todoList/js/chunk-411504aa.88940b4e.js"
+  },
+  {
+    "revision": "eb891d3e202ec97cfbea",
+    "url": "/todoList/js/chunk-vendors.f780150f.js"
+  },
+  {
+    "revision": "dcb2175aeccc5c4079c1e2ac335c7df3",
+    "url": "/todoList/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todoList/robots.txt"
+  }
+]);
